669d02f87622ff5d1b7ecbf1300c6790f6a5ae31
steve <amsteve46@gmail.com>
2021-04-19 17:00:03 +0300

    start the user UI: sign up and login next

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <h1>This is the navbar</h1>
+        </div>
+    );
+}
+ 
+export default Navbar;
