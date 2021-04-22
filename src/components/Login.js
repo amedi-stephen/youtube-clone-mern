9860dca1cde3b24b4debcb39b768935cacc82af9
steve <amsteve46@gmail.com>
2021-04-22 14:39:37 +0300

    modified the form with formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is the login page...</h1>
-        </div>
-    );
-}
- 
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+  });
+  const onSubmit = values => {
+      alert(JSON.stringify(values, null, 2))
+  }
+  return (
+    <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+    >
+      {() => (
+        <Form className="centered-form login-form">
+          <h2>Log in </h2>
+          <div class="form-group">
+            <label htmlFor="email">First Name</label>
+            <Field name="email" placeholder="Enter your email" />
+            <div className="error-message">
+              <ErrorMessage name="email" />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label htmlFor="password">Password</label>
+            <Field name="password" placeholder="Enter your password" />
+            <div className="error-message">
+                <ErrorMessage name="password" />
+            </div>
+          </div>
+
+          <button type="submit">Log In</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
 export default Login;
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,54 +1,77 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const SignupForm = () => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().min(3, "Weak password").required("Required"),
+    confirmPassword: Yup.string().required("Required"),
+  });
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
   return (
-    <form className="centered-form signup-form">
-      <div class="form-group">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="Enter your first name"
-        />
-      </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {() => (
+        <Form className="centered-form signup-form">
+          <h2>Sign Up</h2>
+          <div class="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" placeholder="Enter your first name" />
+            <div className="error-message">
+              <ErrorMessage name="firstName" />
+            </div>
+          </div>
 
-      <div class="form-group">
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Enter your last name"
-        />
-      </div>
+          <div class="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" placeholder="Enter your last names" />
+            <div className="error-message">
+              <ErrorMessage name="lastName" />
+            </div>
+          </div>
 
-      <div class="form-group">
-        <label htmlFor="email">First Name</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-        />
-      </div>
+          <div class="form-group">
+            <label htmlFor="email">Email</label>
+            <Field name="email" placeholder="Enter email" />
+            <div className="error-message">
+              <ErrorMessage name="email" />
+            </div>
+          </div>
 
-      <div class="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-      </div>
+          <div class="form-group">
+            <label htmlFor="password">Password</label>
+            <Field name="password" placeholder="Enter your password" />
+            <div className="error-message">
+              <ErrorMessage name="password" />
+            </div>
+          </div>
 
-      <div class="form-group">
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm your password"
-        />
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+          <div class="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <Field name="confirmPassword" placeholder="Confirm password" />
+            <div className="error-message">
+              <ErrorMessage name="password" />
+            </div>
+          </div>
+          <button type="submit">Sign Up</button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
